6a24900fc03504d4b6715a37f86af995cd9f5deb
n1mra <167897729+n1mra@users.noreply.github.com>
2024-04-23 21:51:41 +0100

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ import csv
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return render_template('index.html') 
+
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
